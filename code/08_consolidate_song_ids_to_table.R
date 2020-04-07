1f2b8c56e7c1fee1c1e6a273227af585724edd4d
Mario Becerra <mbecerracontreras@gmail.com>
2020-04-07 19:20:06 +0200

    Added comments to code

--- code/08_consolidate_song_ids_to_table.R
+++ code/08_consolidate_song_ids_to_table.R
@@ -15,35 +15,44 @@ dir.create(here(log_folder))
 # Iterate in a parallel way on all files
 song_info = mclapply(1:length(index_files), function(core_id){
   
+  # Each core has its own log file
   log_file = here(log_folder, paste0("log_read_rds_", core_id, ".log"))
   file.create(log_file)
   
+  # The indices of filenames corresponding to this core
   core_indices = index_files[[core_id]]
+  
+  # Iterate over all files in "core_indices"
+  
   core_df = map_df(seq_along(core_indices), function(i){
-    
+  
+    # Print progress on log file  
     if(i %% 100 == 1) {
       cat("Doing file", i, "out of", length(core_indices), "\n",
           file = log_file,
           append = T)
     }
     
+    # Index of file to read
     n_file_i = core_indices[i]
     
+    # Name of file to read
     file_name_i = song_files[i]
-    cat("File ", comma(i), " of ", comma(n_files), " (", basename(file_name_i), ")\n",
-        sep = "")
     
+    # Read file and transform all columns to character because some are factors
     song_info = readRDS(file_name_i) %>% 
       as_tibble() %>% 
       mutate_all(~as.character(.))
     
     return(song_info)
   })
-  
   return(core_df)
   
 }) %>% 
   bind_rows()
+
+
+
 
 
 # # In a non-parallel way
